4c5f50ee4770c96a730ad2dfbaea490aee1769a2
Varun <varunmj99@gmail.com>
2024-03-04 23:44:35 +0530

    revise

---
+++ mar-24/04-03.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/bag-of-tokens/description/?envType=daily-question&envId=2024-03-04
+
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+        sort(tokens.begin(), tokens.end());
+        int left = 0, right = tokens.size()-1;
+        int maxScore = 0, currScore = 0;
+        while(left <= right){
+            if(power >= tokens[left]){
+                currScore++;
+                power -= tokens[left++];
+            }else{
+                currScore--;
+                power += tokens[right--];
+            }
+            if(currScore < 0)break;
+            maxScore = max(maxScore, currScore);
+        }
+        return maxScore;
+    }
+};
