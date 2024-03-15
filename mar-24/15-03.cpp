a58336c0aff8b342d86889247a4217e1c40f6d2d
Varun <varunmj99@gmail.com>
2024-03-15 23:04:29 +0530

    revise

---
+++ mar-24/15-03.cpp
@@ -0,0 +1,26 @@
+//https://leetcode.com/problems/product-of-array-except-self/description/?envType=daily-question&envId=2024-03-15
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        
+        std::vector<int> prefix(n, 1);
+        std::vector<int> suffix(n, 1);
+        
+        for (int i = 1; i < n; ++i) {
+            prefix[i] = prefix[i - 1] * nums[i - 1];
+        }
+        
+        for (int i = n - 2; i >= 0; --i) {
+            suffix[i] = suffix[i + 1] * nums[i + 1];
+        }
+        
+        std::vector<int> answer(n);
+        for (int i = 0; i < n; ++i) {
+            answer[i] = prefix[i] * suffix[i];
+        }
+        
+        return answer;
+    }
+};
